b8b856c12e7e90cbfa61d648654f534047057dd0
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-09 23:38:51 +0000

    Function receives string and returns object with count of letters from that string

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+// function takes sentence string
+// returns count of the letters in that sentence
+
+
+const countLetters = (sentenceToCount) => {
+  let sentenceWithoutSpaces = sentenceToCount.split(" ").join("");
+  let letterCountObject = {};
+
+  for (let char of sentenceWithoutSpaces) {
+
+    if (letterCountObject[char] == undefined) {
+      letterCountObject[char] = 1;
+    } else {
+      letterCountObject[char] += 1;
+    }
+  }
+  return letterCountObject;
+};
+
+console.log(countLetters("lighthouse in the house"));
