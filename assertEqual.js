0b2e19c68b70a5c910c972d0005ac468d560ef7c
double-slide <93881358+double-slide@users.noreply.github.com>
2022-03-14 21:04:37 +0000

    Refactored to remove test code and instead use /test directory

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("iaan", "iaan");
-assertEqual(2, 5);
+module.exports = assertEqual;
